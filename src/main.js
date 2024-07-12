43598e87aac906424453cc97e9c3dce5c9e34153
Robbert-jan <robbertjan007@hotmail.com>
2024-07-12 16:16:25 +0200

    added projectbutton

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 // this is going to be the main js module to start loading things
 import {layoutConCheck, loadRegularGrid, createSideBar, createTaskPanel, loadProjectGrid} from "./layout";
+import {addProjectButton, projectsCheck} from "./projects";
 import "./style.css";
 
 
 layoutConCheck();
+projectsCheck(); 
 createSideBar()
 createTaskPanel();
 loadRegularGrid();
+addProjectButton();
 
+
+// This is making the sidebar responding to projects or to tasks
 
 const sidebar = document.querySelector(".sideBar");
 sidebar.addEventListener("click", loadProjectGrid);
 sidebar.addEventListener("mouseout", loadRegularGrid);
+
+
 
 
 
