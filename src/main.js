5a454744f1f8fb09dcb95a146ccf7d94ce9b7a56
Robbert-jan <robbertjan007@hotmail.com>
2024-07-17 12:04:17 +0200

    added some tests for the Project class

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // this is going to be the main js module to start loading things
 import {layoutConCheck, loadRegularGrid, createSideBar, createTaskPanel, loadProjectGrid} from "./layout";
-import {addProjectButton, projectsCheck} from "./projects";
+import {addProjectButton, projectsCheck, Project} from "./projects";
 import "./style.css";
 
 
@@ -15,15 +15,23 @@ addProjectButton();
 // This is making the sidebar responding to projects or to tasks
 
 const sidebar = document.querySelector(".sideBar");
-sidebar.addEventListener("click", loadProjectGrid);
-sidebar.addEventListener("mouseout", loadRegularGrid);
+const ProjectButton = document.querySelector(".projectButton");
+sidebar.addEventListener("click", loadProjectGrid, true);
+sidebar.addEventListener("mouseleave",loadRegularGrid);
+ProjectButton.addEventListener("click", (e) => {
+    console.log(`${e.currentTarget.classList} is clicked`);
+}); 
 
 
+let test = "banana"
+const checksome = new Project("Network");
 
+console.log(checksome);
+console.log(checksome.age);
 
-
-
-
+checksome.age.addEventListener("change",() => {
+    console.log(checksome.age);
+})
 
 
 
